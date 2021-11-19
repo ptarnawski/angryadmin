5d7bcc489138979656f27fcafaa8ab1d3812c533
ptarnawski <84711553+ptarnawski@users.noreply.github.com>
2021-11-19 14:42:06 +0000

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,6 +4,7 @@ region = "us-east-2"
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.medium"
+  count = 5
   key_name = "angry-admin-key"
 
   tags = {
