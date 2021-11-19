44081b202a0088d13e2d5ccd3500b9709e85f3eb
ptarnawski <84711553+ptarnawski@users.noreply.github.com>
2021-11-19 11:43:23 +0000

    Create server.tf

---
+++ server.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+region = "us-east-2"
+}
+resource "aws_instance" "myawsserver" {
+  ami = "ami-077e31c4939f6a2f3"
+  instance_type = "t2.micro"
+  key_name = "gds-import"
+
+  tags = {
+    Name = "Gagan-devops-ec2-instance-v2"
+    env = "development"
+  }
+  provisioner "local-exec" {
+    command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /root/inv"
+  }
+}
