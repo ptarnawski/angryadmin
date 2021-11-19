34fd4e12fbef5e2125316d72bca47090897abd65
ptarnawski <84711553+ptarnawski@users.noreply.github.com>
2021-11-19 15:08:03 +0000

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,13 +4,14 @@ region = "us-east-2"
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.medium"
-  count = 5
+  count = 3
   key_name = "angry-admin-key"
 
   tags = {
     Name = "AngrySysOps-Infra"
     env = "development"
     env = "prod"
+    env = "test"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /root/inv"
